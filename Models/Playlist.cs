94f427519da16f49f37716204ae06b5a440adb47
Zer0-01 <anas.zulkifli2001@gmail.com>
2024-10-06 14:40:25 +0800

    first commit

---
+++ Models/Playlist.cs
@@ -0,0 +1,17 @@
+using System.Text.Json.Serialization;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+public class Playlist
+{
+
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
+    public string? Id { get; set; }
+
+    public string username { get; set; } = null!;
+
+    [BsonElement("items")]
+    [JsonPropertyName("items")]
+    public List<string> movieIds { get; set; } = null!;
+}

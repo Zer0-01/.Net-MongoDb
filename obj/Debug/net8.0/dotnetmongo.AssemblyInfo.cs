25601599d62de09582ba18db82c4b0f092b830fe
Zer0-01 <anas.zulkifli2001@gmail.com>
2024-10-07 21:55:56 +0800

    feat: add book entity model

--- obj/Debug/net8.0/dotnetmongo.AssemblyInfo.cs
+++ obj/Debug/net8.0/dotnetmongo.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("dotnetmongo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+94f427519da16f49f37716204ae06b5a440adb47")]
 [assembly: System.Reflection.AssemblyProductAttribute("dotnetmongo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("dotnetmongo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

b8062fe67ea4d4da15bd8015cba6b58099754eae
Zer0-01 <anas.zulkifli2001@gmail.com>
2024-10-07 22:30:58 +0800

    chore: reset project

--- Models/MongoDBSettings.cs
+++
@@ -1,6 +0,0 @@
-public class MongoDBSettings {
-
-    public string ConnectionURI { get; set; } = null!;
-    public string DatabaseName { get; set; } = null!;
-    public string CollectionName { get; set; } = null!;
-}

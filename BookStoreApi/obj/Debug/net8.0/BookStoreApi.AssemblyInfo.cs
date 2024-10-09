23da09d95e8b0ad10f2b9dab7cd8767297873fa2
Zer0-01 <anas.zulkifli2001@gmail.com>
2024-10-09 22:09:13 +0800

    feat: add mongodb driver

--- BookStoreApi/obj/Debug/net8.0/BookStoreApi.AssemblyInfo.cs
+++ BookStoreApi/obj/Debug/net8.0/BookStoreApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BookStoreApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+962722940e4e8dbbe39d6afdd874d45603241aed")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c74f4b9911dfc297d4b97a9c67bf76342d6f4671")]
 [assembly: System.Reflection.AssemblyProductAttribute("BookStoreApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BookStoreApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
